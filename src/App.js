5b77ee9e146878d0a07bc694be8ba60ab04c5f73
isrivastav99 <ishaan@gmail.com>
2020-11-25 00:19:07 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import Calendar from './components/Calendar'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Calendar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
