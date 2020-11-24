06301fa458eedfb2e2f99c34a879d62513e85773
isrivastav99 <ishaan@gmail.com>
2020-11-25 00:56:50 +0530

    fixed more date issues

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import logo from './logo.svg';
-import Calendar from './components/Calendar'
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Calendar />
-      </header>
-    </div>
-  );
+import React from "react";
+
+import Calendar from "./components/Calendar";
+
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <div id="logo">
+            <span className="icon">date_range</span>
+            <span>
+              react<b>calendar</b>
+            </span>
+          </div>
+        </header>
+        <main>
+          <Calendar />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
