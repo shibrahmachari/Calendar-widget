bf3b5d11e8c711ea2c222a1181605cecc9b09d5e
isrivastav99 <ishaan@gmail.com>
2020-11-26 03:55:11 +0530

    left cal

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-
 import React from "react";
-
+//import { Container, Row, Col } from 'reactstrap';
 import Calendar from "./components/Calendar/Calendar";
 import Navbar from "./components/Navbar/Nav";
+import dp from "./dp.jpg";
 import "./App.css";
 
 class App extends React.Component {
@@ -10,15 +10,30 @@ class App extends React.Component {
     return (
       <div className="App">
         <header>
-          <div id="logo">
+          <div className=" top-nav">
+            <div className="name-left">
+              <h2>Franks Planner</h2>
+            </div>
+            <div className="user-right">
+              <h5>Frank Guerrero</h5>
+              <img src={dp} className="user-dp"></img>
+            </div>
           </div>
+          {/*  */}
         </header>
         <main>
-          <div className = "nav">
-          <Navbar />
+          <div className="nav">
+            <Navbar />
           </div>
-          
-          <Calendar />
+          <div className="left-calendar">
+            <div style={{ height: "500px" }}>
+              <Calendar />
+            </div>
+
+            <button>hi</button>
+            <button>hi</button>
+            <button>hi</button>
+          </div>
         </main>
       </div>
     );
--- src/components/Navbar/Nav.js
+++ src/components/Navbar/Nav.js
@@ -1,52 +1,69 @@
-
 import React from "react";
-import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
-import 'font-awesome/css/font-awesome.min.css';
+import SideNav, { NavItem, NavIcon, NavText } from "@trendmicro/react-sidenav";
+import "font-awesome/css/font-awesome.min.css";
 // Be sure to include styles at some point, probably during your bootstraping
-import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+import "@trendmicro/react-sidenav/dist/react-sidenav.css";
 
-class Navbar extends React.Component{
-    render(){
-        return(
-            <div className = "navbar">
-<SideNav
-    onSelect={(selected) => {
-        // Add your code here
-    }}
->
-    <SideNav.Toggle />
-    <SideNav.Nav defaultSelected="home">
-        <NavItem eventKey="home">
-            <NavIcon>
-                <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
-            </NavIcon>
-            <NavText>
-                Home
-            </NavText>
-        </NavItem>
-        <NavItem eventKey="charts">
-            <NavIcon>
-                <i className="fa fa-fw fa-line-chart" style={{ fontSize: '1.75em' }} />
-            </NavIcon>
-            <NavText>
-                Charts
-            </NavText>
+class Navbar extends React.Component {
+  render() {
+    return (
+      <SideNav
+        onSelect={(selected) => {
+          // Add your code here
+        }}
+      >
+        <div className="navbar">
+          <SideNav.Nav defaultSelected="home">
+            <NavItem eventKey="charts">
+              <NavIcon>
+                <i
+                  className="fa fa-fw fa-fire"
+                  style={{
+                    fontSize: "1.75em",
+                    color: "#dd3e21",
+                    padding: "6px",
+                  }}
+                />
+              </NavIcon>
+              <NavText>Charts</NavText>
+            </NavItem>
+            <NavItem eventKey="charts">
+              <NavIcon>
+                <i
+                  className="fa fa-fw fa-line-chart"
+                  style={{
+                    fontSize: "1.75em",
+                    color: "#fff",
+                    background: "#dd3e21",
+                    padding: "6px",
+                  }}
+                />
+              </NavIcon>
+              <NavText>Charts</NavText>
+            </NavItem>
+
             <NavItem eventKey="charts/linechart">
-                <NavText>
-                    Line Chart
-                </NavText>
+              <NavIcon>
+                <i
+                  className="fa fa-fw fa-home"
+                  style={{ fontSize: "1.75em", color: "#bbc5cf" }}
+                />
+              </NavIcon>
+              <NavText>Line Chart</NavText>
             </NavItem>
             <NavItem eventKey="charts/barchart">
-                <NavText>
-                    Bar Chart
-                </NavText>
+              <NavIcon>
+                <i
+                  className="fa fa-fw fa-home"
+                  style={{ fontSize: "1.75em", color: "#bbc5cf" }}
+                />
+              </NavIcon>
+              <NavText>Bar Chart</NavText>
             </NavItem>
-        </NavItem>
-    </SideNav.Nav>
-</SideNav>
-            </div>
-            
-        );
-    }
+          </SideNav.Nav>
+        </div>
+      </SideNav>
+    );
+  }
 }
 export default Navbar;
